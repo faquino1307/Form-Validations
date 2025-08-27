b717ed1eb34a86c979043f2b1745656377800e4a
Fernando Aquino Terrazas <fernando.aquino@theergogroup.com>
2025-08-26 21:46:45 -0600

    Cambiar form por div, aprender a usar el addEventListener para saber cuando el DOM se cargo, aprender a escuchar un evento onclick por el atributo de un boton

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,26 @@
-var miCadena = 'Mi cadena conm "comillas simples"';
+var miCadena = 'Objetos Importantes en JS';
 var miCadena2 = "Mi cadena con 'comillas dobles'";
 console.log('Hola mundo!');
 console.log(miCadena);
-console.log(miCadena2);
+console.log(document); //DOM Document Object Model
+console.log(window);
+
+function main(){
+    console.log("All html are Loaded: The document is fully loaded and parsed!");
+
+    var nombre = document.querySelector('input[name=nombre]');
+    var nombreByID = document.getElementById('nombre');
+
+    console.log(nombre);
+    console.log(nombreByID);
+}
+
+function send(){
+    console.log('click');
+}
+
+document.addEventListener("DOMContentLoaded",main);
+
+
+    
+
